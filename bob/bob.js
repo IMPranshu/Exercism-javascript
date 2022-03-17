48e8a3852db95d7d564de75a9f096de2cce13625
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-17 15:38:36 +0530

    One test passing

--- bob/bob.js
+++ bob/bob.js
@@ -4,5 +4,21 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
+  message.trim;
+  let len = message.length;
+  if(message[len -1] == '?' ){
+    message.splice(len-1, 1);
+    message.splice(0,1);
+    if(message == message.toLowerCase()){
+      return "Sure."
+
+    }else if (message == message.toUpperCase()){
+      return "Calm down, I know what I'm doing!";
+    }
+  }else if(message == message.toUpperCase()){
+    return "Whoa, chill out!";
+  }else if(message.toLowerCase() == "bob"){
+    return "Fine. Be that way!";
+  }
+  return "Whatever.";
 };
