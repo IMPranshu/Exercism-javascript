4250db083d98b49b8c7d7ad9c95fc1a42ff5d2cd
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-17 23:31:48 +0530

    message with no letters remaining

--- bob/bob.js
+++ bob/bob.js
@@ -4,21 +4,20 @@
 //
 
 export const hey = (message) => {
-  message.trim;
-  let len = message.length;
+  let len = message.trim().length;
+  if (len == 0){
+    return "Fine. Be that way!";
+  }
   if(message[len -1] == '?' ){
-    message.splice(len-1, 1);
-    message.splice(0,1);
-    if(message == message.toLowerCase()){
-      return "Sure."
-
-    }else if (message == message.toUpperCase()){
+    message.slice(len-1, 1);
+    message.slice(0,1);
+    if (message == message.toUpperCase()){
       return "Calm down, I know what I'm doing!";
     }
-  }else if(message == message.toUpperCase()){
+    return "Sure.";
+  }
+  else if(message == message.toUpperCase()){
     return "Whoa, chill out!";
-  }else if(message.toLowerCase() == "bob"){
-    return "Fine. Be that way!";
   }
   return "Whatever.";
 };
