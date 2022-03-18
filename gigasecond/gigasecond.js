88cbb2138ecd3bb9ab0bbcfba14630ec563c6f40
P K Agrawal <pranshukumar1999@gmail.com>
2022-03-18 23:20:37 +0530

    task done

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -2,7 +2,9 @@
 // This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+export const gigasecond = (date) => {
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+  let cloneDate = new Date(date.valueOf());
+  cloneDate.setUTCSeconds(cloneDate.getSeconds() + 1000000000);
+  return cloneDate;
 };
